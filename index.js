df153031977ca996f4cdd417c310a8440b2cfc06
Nguyễn Hưng Khánh <nhk020996@gmail.com>
2020-02-24 18:05:38 +0700

    update remove sepcial charecters

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const fs = require("fs");
 const parse = require("csv-parse");
-const download = require("./download");
+const request = require("request");
 
-const inputFile = "data.csv";
+const file = "data.csv";
+const folder = "images";
 
-if (!fs.existsSync('images')){
-  fs.mkdirSync('images');
+if (!fs.existsSync(folder)){
+  fs.mkdirSync(folder);
 }
 
-fs.createReadStream(inputFile)
+const download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body) {
+    request(uri)
+      .pipe(fs.createWriteStream(folder + '/' + filename))
+      .on('close', callback)
+  });
+};
+
+fs.createReadStream(file)
   .pipe(parse({delimiter: ','}))
   .on('data', function(row) {
     try {
-      download(row[1], row[0] + '.png', () => {
-        console.log(`Downloaded ${row[0]}`)
+      download(row[1], row[0].replace(/[^a-zA-Z0-9 ]/g, '') + '.png', () => {
+        console.log(`Downloaded ${row[0].replace(/[^a-zA-Z0-9 ]/g, '')}`)
       }); 
     } catch (error) {
       console.log(error)
